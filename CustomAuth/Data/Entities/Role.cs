3b735131ff4a24178a87a2672e9de2fe41e145be
Joshua Tucker <the1jtux@gmail.com>
2020-09-10 15:39:22 -0400

    Scaffolded out Data Context and User/Role classes

---
+++ CustomAuth/Data/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomAuth.Data.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public virtual List<User> Users { get; set; } = new List<User>();
+    }
+}
